5952fb0d356b60480336e64d78bc4f569fc91ca3
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-06 12:08:07 +0600

    feature card shadow color change

--- src/Components/FeatureCard.jsx
+++ src/Components/FeatureCard.jsx
@@ -3,7 +3,7 @@
 const FeatureCard = ({ feature }) => {
     const { title, img, description } = feature || {}
     return (
-        <div className="card card-compact bg-base-100 shadow-2xl rounded-none">
+        <div className="card card-compact bg-base-100 shadow-2xl rounded-none hover:shadow-black">
             <figure><img src={img} alt="img" className="h-[300px] w-[300px]" /></figure>
             <div className="card-body">
                 <h2 className="card-title font-bold text-center">{title}</h2>
