4c9e6d92788da40ed58d64d561633f0721e16f71
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-08 15:01:40 +0600

    data updating succesfully

--- src/Pages/Update.jsx
+++ src/Pages/Update.jsx
@@ -6,6 +6,8 @@ import { useLoaderData } from "react-router-dom";
 const Update = () => {
 
     const { user } = useContext(AuthContext);
+    const assign = useLoaderData();
+    const { _id, title, category, photo, mark, description, date, thumbnail } = assign || {}
 
     const handleUpdateAssignment = event => {
         event.preventDefault();
@@ -43,126 +45,124 @@ const Update = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                if (data.modifiedCount >0) {
+                if (data.modifiedCount > 0) {
                     Swal.fire(
                         'Success',
                         'Assignment Update Successfull',
                         'success'
                     )
-            }
+                }
             })
 
     }
-    
-    const assignment = useLoaderData();
-    const {_id, title, category,photo, mark, description, date, thumbnail} = assignment || {}
-    
-
-        return (
-            <div className=" bg-[#F4F3F0] p-8 md:p-24">
-                <h2 className="text-3xl text-center font-medium">Update Assignment</h2>
-                <form onSubmit={handleUpdateAssignment}>
-                    {/* name category */}
-                    <div className="md:flex gap-5">
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Title</span>
-                            </label>
-                            <label className="input-group">
-                                <input type="text" name="title" defaultValue={title} placeholder="Enter title" className="input input-bordered w-full" />
-                            </label>
-                        </div>
-
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Category</span>
-                            </label>
-                            <label className="input-group">
-                                <select name="category_name" defaultValue={category} className="input input-bordered w-full" >
-                                    <option disabled selected>Select Category</option>
-                                    <option>Easy</option>
-                                    <option>Medium</option>
-                                    <option>Hard</option>
-                                </select>
-                            </label>
-
-                        </div>
-                    </div>
-                    {/* price */}
-                    <div className="md:flex  gap-5">
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Date</span>
-                            </label>
-                            <label className="input-group">
-                                <input type="date" name="date" defaultValue={date} placeholder="Enter Product price" className="input input-bordered w-full" />
-                            </label>
-                        </div>
-
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Mark</span>
-                            </label>
-                            <label className="input-group">
-                                <input type="text" name="mark" defaultValue={mark}  placeholder="Assignment Mark" className="input input-bordered w-full" />
-                            </label>
-                        </div>
 
 
-                    </div>
 
-                    {/* email  */}
-                    <div className="md:flex  gap-5">
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <label className="input-group">
-                                <input type="text" name="email" defaultValue={user?.email} className="input input-bordered w-full" />
-                            </label>
-                        </div>
-
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Thumbnail</span>
-                            </label>
-                            <label className="input-group">
-                                <input type="text" name="thumbnail" defaultValue={thumbnail} placeholder="Thumbnail" className="input input-bordered w-full" />
-                            </label>
-                        </div>
-
-
-                    </div>
-
-                    {/*deatails */}
-
-                    <div className="form-control w-full">
+    return (
+        <div className=" bg-[#F4F3F0] p-8 md:p-24">
+            <h2 className="text-3xl text-center font-medium">Update Assignment</h2>
+            <form onSubmit={handleUpdateAssignment}>
+                {/* name category */}
+                <div className="md:flex gap-5">
+                    <div className="form-control md:w-1/2">
                         <label className="label">
-                            <span className="label-text">Description</span>
+                            <span className="label-text">Title</span>
                         </label>
                         <label className="input-group">
-                            <input type="text" name="description" defaultValue={description}  placeholder="Type description" className="input input-bordered w-full" />
+                            <input type="text" name="title" defaultValue={title} placeholder="Enter title" className="input input-bordered w-full" />
                         </label>
                     </div>
 
-                    {/* Photo url */}
-                    <div className="form-control w-full">
+                    <div className="form-control md:w-1/2">
                         <label className="label">
-                            <span className="label-text">Photo</span>
+                            <span className="label-text">Category</span>
                         </label>
-                        <label className="input-group"> 
-                            <input type="text" name="photo" defaultValue={photo} placeholder="Enter photo URL" className="input input-bordered w-full" />
+                        <label className="input-group">
+                            <select name="category_name" defaultValue={category} className="input input-bordered w-full" >
+                                <option disabled selected>Select Category</option>
+                                <option>Easy</option>
+                                <option>Medium</option>
+                                <option>Hard</option>
+                            </select>
+                        </label>
+
+                    </div>
+                </div>
+                {/* price */}
+                <div className="md:flex  gap-5">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Date</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="date" name="date" defaultValue={date} placeholder="Enter Product price" className="input input-bordered w-full" />
                         </label>
                     </div>
 
-                    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  p-3 my-5 rounded-lg text-center">
-                        <input type="submit" value="Create Assignment" />
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Mark</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="mark" defaultValue={mark} placeholder="Assignment Mark" className="input input-bordered w-full" />
+                        </label>
                     </div>
-                </form>
 
-            </div>
-        );
-        
-    };
 
-    export default Update;
+                </div>
+
+                {/* email  */}
+                <div className="md:flex  gap-5">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="email" defaultValue={user?.email} className="input input-bordered w-full" />
+                        </label>
+                    </div>
+
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Thumbnail</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="thumbnail" defaultValue={thumbnail} placeholder="Thumbnail" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+
+
+                </div>
+
+                {/*deatails */}
+
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">Description</span>
+                    </label>
+                    <label className="input-group">
+                        <input type="text" name="description" defaultValue={description} placeholder="Type description" className="input input-bordered w-full" />
+                    </label>
+                </div>
+
+                {/* Photo url */}
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">Photo</span>
+                    </label>
+                    <label className="input-group">
+                        <input type="text" name="photo" defaultValue={photo} placeholder="Enter photo URL" className="input input-bordered w-full" />
+                    </label>
+                </div>
+
+                <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  p-3 my-5 rounded-lg text-center">
+                    <input type="submit" value="Update Assignment" />
+                </div>
+            </form>
+
+        </div>
+    );
+
+};
+
+export default Update;
